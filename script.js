92dd11fb48174f5b9c42568714449cce2b5daa78
Caylin Vu <caylinvu@gmail.com>
2023-01-10 13:43:04 -0600

    Add slider range label

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const gridContainer = document.querySelector('.grid-container');
 const slider = document.querySelector('.slider');
+const rangeLabel = document.querySelector('.range-label');
+
+// remove child nodes from container
+
+function deleteGrid() {
+    let child = gridContainer.lastElementChild;
+    while (child) {
+        gridContainer.removeChild(child);
+        child = gridContainer.lastElementChild;
+    }
+}
+
+// update the slider range label when slider is moved
+
+function updateRangeLabel() {
+    rangeLabel.textContent = `${slider.value} x ${slider.value}`;
+}
+
 
 // create grid from slider input
 
@@ -15,19 +33,19 @@ function createGrid() {
 
         gridContainer.appendChild(gridSquare);
     }
-}
 
-// remove children nodes from container
-
-function deleteGrid() {
-    let child = gridContainer.lastElementChild;
-    while (child) {
-        gridContainer.removeChild(child);
-        child = gridContainer.lastElementChild;
-    }
+    updateRangeLabel();
 }
 
 
 createGrid();
 
 slider.addEventListener('click', createGrid);
+
+// add default (black) drawing function
+
+// add clear function
+
+// add RGB drawing function
+
+// add shading function
