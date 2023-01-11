93a630d81c094621a4989f02c7c5dff903740e64
Caylin Vu <caylinvu@gmail.com>
2023-01-11 12:43:17 -0600

    Add drawing in random RGB colors

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const gridContainer = document.querySelector('.grid-container');
 const slider = document.querySelector('.slider');
 const rangeLabel = document.querySelector('.range-label');
+const blackButton = document.querySelector('.black-button');
+const rgbButton = document.querySelector('.rgb-button');
+const shadingButton = document.querySelector('.shading-button');
 const clearButton = document.querySelector('.clear-button');
 
 let isDragging = false;
-
 
 // remove child nodes from container
 
@@ -16,13 +18,32 @@ function deleteGrid() {
     }
 }
 
-
 // update the slider range label when slider is moved
 
 function updateRangeLabel() {
     rangeLabel.textContent = `${slider.value} x ${slider.value}`;
 }
 
+// clear grid when clear button is clicked
+
+function clearGrid() {
+    clearButton.addEventListener('click', () => {
+        let nodes = gridContainer.children;
+        for (let i = 0; i < nodes.length; i++) {
+            nodes[i].style.backgroundColor = 'lightgrey';
+        }
+    });    
+}
+
+// generate random color
+function generateRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for(let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
 // create grid from slider input
 
@@ -38,7 +59,7 @@ function createGrid() {
 
         gridContainer.appendChild(gridSquare);
 
-        // click and drag to draw on etch a sketch
+        // click and drag to draw on etch a sketch (default color - black)
 
         gridSquare.addEventListener('mousedown', () => {
             isDragging = true;
@@ -54,18 +75,49 @@ function createGrid() {
         gridSquare.addEventListener('mouseup', () => {
             isDragging = false;
         });
+
+        // change drawing color back to black after pressing black button
+
+        blackButton.addEventListener('click', () => {
+            gridSquare.addEventListener('mousedown', () => {
+                isDragging = true;
+                gridSquare.style.backgroundColor = 'black';
+            });
+
+            gridSquare.addEventListener('mouseover', () => {
+                if (isDragging) {
+                    gridSquare.style.backgroundColor = 'black';
+                }
+            });
+    
+            gridSquare.addEventListener('mouseup', () => {
+                isDragging = false;
+            });
+        });
+
+        // change drawing color to RGB after pressing RGB button
+
+        rgbButton.addEventListener('click', () => {
+            gridSquare.addEventListener('mousedown', () => {
+                isDragging = true;
+                gridSquare.style.backgroundColor = generateRandomColor();
+            });
+
+            gridSquare.addEventListener('mouseover', () => {
+                if (isDragging) {
+                    gridSquare.style.backgroundColor = generateRandomColor();
+                }
+            });
+    
+            gridSquare.addEventListener('mouseup', () => {
+                isDragging = false;
+            });
+        });
+
     }
 
     updateRangeLabel();
-
-    // *** see if can move gridSquare event listeners down using children nodes
-
-    clearButton.addEventListener('click', () => {
-        let nodes = gridContainer.children;
-        for (let i = 0; i < nodes.length; i++) {
-            nodes[i].style.backgroundColor = 'lightgrey';
-        }
-    });
+    clearGrid();
 }
 
 
@@ -73,10 +125,8 @@ createGrid();
 
 slider.addEventListener('click', createGrid);
 
-// add default (black) drawing function
-
-// add clear function
-
 // add RGB drawing function
 
 // add shading function
+
+// add UI
